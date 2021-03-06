e432aa6bda54d06672c78fc0ac6876a2b5d04d0e
Kamil McCabe <kamil.mccabe@ojassociates.com>
2021-03-06 15:40:38 +0000

    img and glitch

--- js/site.js
+++ js/site.js
@@ -1,5 +1,7 @@
 import tree from './sketches/tree.js';
 import squares from './sketches/squares.js';
+import img_glitch from './sketches/img_glitch.js';
 
+let img_glitch_sketch = new p5(img_glitch, 'img_glitch');
 let tree_sketch = new p5(tree, 'tree');
 let square_sketch = new p5(squares, 'squares');
---
+++ js/sketches/img_glitch.js
@@ -0,0 +1,42 @@
+const img_glitch = (p) => {
+
+    let glitch;
+    let cnv;
+
+    p.setup = () => {
+        cnv = p.createCanvas(900, 550);
+        p.imageMode(p.CENTER);
+
+        glitch = new Glitch(p);
+        p.loadImage('../../assets/imgs/img1.jpeg', function (im) {
+            glitch.loadImage(im);
+        });
+        //glitch.limitStart = 0.3;
+    }
+    let mx = 0,
+        my = 0;
+
+    p.draw = () => {
+        p.background(255);
+        glitch.resetBytes();
+
+        // if (p.mouseIsPressed) {
+        //     glitch.replaceBytes(100, 104);
+        // }
+
+        let byte1 = p.random(100,104);
+        let byte2 = p.random(100,104);
+        //glitch.replaceBytes(byte1, byte2); // swap all decimal byte 100 for 104
+        glitch.replaceBytes(100, 104); // swap all decimal byte 100 for 104
+        //glitch.replaceBytes(slider.value()); // swap all decimal byte 100 for 104
+        glitch.randomBytes(1);
+
+
+        glitch.buildImage();
+        p.image(glitch.image, p.width / 2, p.height / 2, p.width, p.height)
+
+    }
+
+}
+
+export default img_glitch;
