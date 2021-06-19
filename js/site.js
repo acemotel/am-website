b454b4945a095e51e8fb086870c6569294f4d24c
Kamil <kamil@kamils-mbp-2.mynet>
2021-06-19 20:42:03 +0100

    layout, skew, code, favicon

--- js/site.js
+++ js/site.js
@@ -4,9 +4,9 @@ import img_glitch from './sketches/img_glitch.js';
 
 window.navhover = false;
 
-let img_glitch_sketch = new p5(img_glitch, 'img_glitch');
+//let img_glitch_sketch = new p5(img_glitch, 'img_glitch');
 let tree_sketch = new p5(tree, 'tree');
-let square_sketch = new p5(squares, 'squares');
+//let square_sketch = new p5(squares, 'squares');
 
 const play = () => {
     console.log('click')
@@ -29,11 +29,28 @@ const nav_out = () => {
     window.navhover = false;
 }
 
-document.getElementById("play").addEventListener("click", play);
-document.getElementById("stop").addEventListener("click", stop);
 
-const nav_links = document.querySelectorAll(".nav a");
-nav_links.forEach(item => {
-    item.addEventListener("mouseenter", nav_enter);
-    item.addEventListener("mouseout", nav_out);
+
+let amCode = document.getElementById("code");
+// Randomly generate the code AMxxx
+setInterval(() => {
+    let value = Math.floor(100 + Math.random() * 900);
+    amCode.innerHTML = "AM" + value;
+}, 100);
+
+// give each element a random skew
+let elementSkew = document.getElementsByClassName("am");
+[].forEach.call(elementSkew, function (el) {
+    let valueX = Math.ceil(Math.random() * 2) * (Math.round(Math.random()) ? 1 : -1)
+    let valueY = Math.ceil(Math.random() * 2) * (Math.round(Math.random()) ? 1 : -1)
+    el.style.transform = `skew(${valueX}deg, ${valueY}deg)`;
 });
+
+// document.getElementById("play").addEventListener("click", play);
+// document.getElementById("stop").addEventListener("click", stop);
+
+// const nav_links = document.querySelectorAll(".nav a");
+// nav_links.forEach(item => {
+//     item.addEventListener("mouseenter", nav_enter);
+//     item.addEventListener("mouseout", nav_out);
+// });
