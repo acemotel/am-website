51e277f04d174662db763e265bf7bb7306cf1453
Kamil <kamil@kamils-mbp-2.mynet>
2021-08-25 10:32:47 +0100

    font/style changes, squares shown on listen

--- js/site.js
+++ js/site.js
@@ -20,10 +20,12 @@ let isPlaying = false;
 document.getElementById("play").onclick = () => {
     if (!isPlaying){
         awotb_play();
+        document.getElementById("squares").style.display = 'block';
         isPlaying = true;
     }
     else {
         awotb_stop();
+        document.getElementById("squares").style.display = 'none';
         isPlaying = false;
     }
 }
@@ -32,10 +34,12 @@ document.body.onkeyup = function(e){
     if(e.keyCode == 32){
         if (!isPlaying){
             awotb_play();
+            document.getElementById("squares").style.display = 'block';
             isPlaying = true;
         }
         else {
             awotb_stop();
+            document.getElementById("squares").style.display = 'none';
             isPlaying = false;
         }
     }
@@ -54,56 +58,16 @@ const nav = document.getElementsByClassName("am");
 if (!isMobile) {
     console.log("no")
     const imgs = document.getElementsByClassName("imgs");
-    const squaresel = document.getElementById("squares");
+    
 
-    for (let i = 1; i < nav.length; i++) {
+    for (let i = 1; i < nav.length-1; i++) {
         nav[i].addEventListener("mouseover", function () {
             imgs[i - 1].style.display = 'block';
-            if (i == 3) {
-                squaresel.style.display = 'block';
-            }
-            // for (let j = 0; j <= i; j++) {
-            //     imgs[j].style.display = 'block';
-            // }
         });
         nav[i].addEventListener("mouseout", function () {
-            // for (let j = 0; j <= i; j++) {
-            //     imgs[j].style.display = 'none';
-            // }
             setTimeout(() => {
                 imgs[i - 1].style.display = 'none';
-                squaresel.style.display = 'none';
-            },100);
+            },50);
         });
     }
 }
-
-function play() {
-    
-}
-
-// let amCode = document.getElementById("code");
-// // Randomly generate the code AMxxx
-// setInterval(() => {
-//     let value = Math.floor(100 + Math.random() * 900);
-//     amCode.innerHTML = "AM" + value;
-// }, 100);
-
-// give each element a random skew
-// [].forEach.call(nav, function (el) {
-//     let valueX = Math.ceil(Math.random() * 2) * (Math.round(Math.random()) ? 1 : -1)
-//     let valueY = Math.ceil(Math.random() * 2) * (Math.round(Math.random()) ? 1 : -1)
-//     el.style.transform = `skew(${valueX}deg, ${valueY}deg)`;
-// });
-
-
-
-
-// document.getElementById("play").addEventListener("click", play);
-// document.getElementById("stop").addEventListener("click", stop);
-
-// const nav_links = document.querySelectorAll(".nav a");
-// nav_links.forEach(item => {
-//     item.addEventListener("mouseenter", nav_enter);
-//     item.addEventListener("mouseout", nav_out);
-// });
